b88ab436fd1694dcb070951fedf1d65b3c6bd09c
Enrique Torres <enriquetorresv652@gmail.com>
2022-11-02 18:54:58 -0700

    ChiCuadrada

---
+++ ChiCuadrada/Program.cs
@@ -0,0 +1,26 @@
+﻿class ChiCuadrada
+{
+    double o, e, cuadrada, resultado;
+    public void Datos()
+    {
+        Console.WriteLine("Ingresar el numero de eventos observados");
+        o = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingresar el numero de eventos esperados");
+        e = double.Parse(Console.ReadLine());
+    }
+    public void Cuadrada()
+    {
+        cuadrada = Math.Pow(o - e, 2);
+        resultado = cuadrada / e;
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        ChiCuadrada c = new ChiCuadrada();
+        c.Datos();
+        c.Cuadrada();
+    }
+}
