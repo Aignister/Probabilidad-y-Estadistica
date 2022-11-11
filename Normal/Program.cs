d14f16dec5a1ed7d3b31dace48ef29db2d22a687
Enrique Torres <enriquetorresv652@gmail.com>
2022-11-10 17:09:54 -0800

    Update Program.cs

--- Normal/Program.cs
+++ Normal/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Normal
+{
+    int x, u, o;
+    double z;
+    public void Datos()
+    {
+        Console.WriteLine("Ingresar X");
+        x=int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingresar u");
+        u=int.Parse(Console.ReadLine());      
+        Console.WriteLine("Ingresar o");
+        o=int.Parse(Console.ReadLine());   
+    }
+}
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
