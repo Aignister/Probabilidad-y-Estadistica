26eaf1db76ebcabf21bd4a9ad59943f537b667a8
Enrique Torres <enriquetorresv652@gmail.com>
2022-11-10 17:09:21 -0800

    Normal

--- F-Fisher/Program.cs
+++ F-Fisher/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Fisher
+{
+    public void Datos()
+    {
+        Console.WriteLine("Ingresar el valor critico");
+        Console.WriteLine("Ingresar grados de libertad del numerador");
+        Console.WriteLine("Ingresar grados de libertad del numerador");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+    }
+}
